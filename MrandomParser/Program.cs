b328dcd2b45609255740743e81c21de8170f94f2
Max Khamrovskyi <maksim36ua@gmail.com>
2016-01-04 21:44:16 +0200

    Uploading parser files from local PC

---
+++ MrandomParser/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using HtmlAgilityPack;
+
+namespace MrandomParser
+{
+    class Program
+    {
+        private static async void Parsing(string website)
+        {
+            try
+            {
+                HttpClient http = new HttpClient();
+                var response = await http.GetByteArrayAsync(website);
+                String source = Encoding.GetEncoding("utf-8").GetString(response, 0, response.Length - 1);
+                source = WebUtility.HtmlDecode(source);
+                
+                HtmlDocument result = new HtmlDocument();
+                result.LoadHtml(source);
+
+                List<HtmlNode> toftitle = result.DocumentNode.Descendants().Where
+                    (x => (x.Name == "div" && x.Attributes["class"] != null && x.Attributes["class"].Value.Contains("col-md-6"))).ToList();
+
+                List<string> id = new List<string>();
+
+                for (int i = 0; i < toftitle.Count; i++)
+                {
+                    var span = toftitle[i].Descendants("h4").ToList();
+                    var link = span[0].Descendants("a").ToList()[0].GetAttributeValue("href", null);
+                    id.Add(link);
+                }
+
+                foreach (var userLink in id)
+                {
+                    var tmp = (userLink.Remove(0,14)).Insert(0, "@");
+                    Console.WriteLine(tmp);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error occured");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter MRandom URL");
+            var url = Console.ReadLine();
+            Parsing(url);
+            Console.Read();
+        }
+    }
+}
---
+++ MrandomParser/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MrandomParser")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MrandomParser")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7a5a2fe7-348e-4139-8b94-44ab1f41aaf4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
